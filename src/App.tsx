1af432476b0e3e5b1bb9726a82aadd7f97538f6a
Michael Simon <michaelsimonaz@gmail.com>
2022-12-23 11:48:36 -0700

    remove data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
-
-import data from '../src/data/data.json'
-
-const allData = data.map((item) => {
-  return (
-    <div key={item.name} className='bg-slate-100 p-2 border-b'>
-      <h2 className='text-lg font-bold'>{item.name}</h2>
-      {item.favorites.map((favorite) => {
-        return (
-            <div key={favorite.name}>{favorite.name}</div>
-        )
-      })
-      }
-    </div>
-  )
-})
-
 function App() {
   return (
-    <div>
-      {allData}
-    </div>
+    <div>App</div>
   )
 }
 
